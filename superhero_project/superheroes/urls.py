91eadcdfe6d1a95d70fe28894576456b9074602d
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-08-27 00:19:02 -0500

    added detail path and add an create template.

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
-  path('',views.index, name = 'index')
+  path('',views.index, name = 'index'),
+  # path('<int:hero_id>/', views.details, name='details' ),
+  path('new/', views.create, name='create')
+
 ]
